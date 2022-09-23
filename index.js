90d063af2616634e7d46cd25ba83b2ac3069be78
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-09-23 20:07:50 +0300

    Add signin via username/password

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const url = require("url");
 const contentFetchers = require("./contentFetchers");
 const postCreators = require("./postCreators");
 
-// env variables
-const lemmyApiUrl = process.env.LEMMY_API_URL;
-const targetUrl = process.env.TARGET_URL;
-const communityId = parseInt(process.env.COMMUNITY_ID, 10);
-const authToken = process.env.AUTH_TOKEN;
+const signIn = async (usernameOrEmail, password) => {
+  const url = `${process.env.LEMMY_API_URL}/user/login`;
+
+  return (
+    await axios.post(url, { username_or_email: usernameOrEmail, password })
+  ).data;
+};
 
 // function: for creating post in Lemmy
-const createLemmyPost = async (data) => {
+const createLemmyPost = async (data, communityId, authToken) => {
   const _data = merge(
     {
       community_id: communityId,
@@ -24,19 +26,34 @@ const createLemmyPost = async (data) => {
 
   console.log(_data);
 
-  const url = `${lemmyApiUrl}/post`;
+  const url = `${process.env.LEMMY_API_URL}/post`;
 
   return await axios.post(url, { ..._data, auth: authToken });
 };
 
 // function: start point
 const main = async () => {
-  if (!lemmyApiUrl || !targetUrl || !communityId || !authToken) {
+  const targetUrl = process.env.TARGET_URL;
+  const communityId = parseInt(process.env.COMMUNITY_ID, 10);
+  const usernameOrEmail = process.env.USERNAME_OR_EMAIL;
+  const password = process.env.PASSWORD;
+
+  if (
+    !process.env.LEMMY_API_URL ||
+    !process.env.TARGET_URL ||
+    !communityId ||
+    !usernameOrEmail ||
+    !password
+  ) {
     throw new Error(
-      "Env variables LEMMY_API_URL, TARGET_URL, COMMUNITY_ID, AUTH_TOKEN are required."
+      "Env variables LEMMY_API_URL, TARGET_URL, COMMUNITY_ID, USERNAME_OR_EMAIL, PASSWORD are required."
     );
   }
 
+  console.log("[info]: Signing in...");
+  const { jwt: authToken } = await signIn(usernameOrEmail, password);
+
+  console.log("[info]: Choosing content fetcher and post creator...");
   const { hostname } = url.parse(targetUrl);
   const fetchContent = contentFetchers[hostname];
   const createPost = postCreators[hostname];
@@ -45,28 +62,26 @@ const main = async () => {
     throw new Error("URL is not supported.");
   }
 
-  console.log("[info]: fetching content");
+  console.log("[info]: Fetching content...");
   const content = await fetchContent(targetUrl);
 
   if (content.length === 0) {
     throw new Error("Content is absent.");
   }
 
-  console.log("[info]: get last post");
-
-  console.log("[info]: creating post");
+  console.log("[info]: Creating post...");
   const post = await createPost(url, content);
 
-  console.log("[info]: push post to Lemmy");
+  console.log("[info]: Push post to API...");
   const {
     data: {
       post_view: { post: postView },
     },
-  } = await createLemmyPost(post);
+  } = await createLemmyPost(post, communityId, authToken);
 
-  console.log("[info]: OK");
+  console.log("[info]: Done");
   console.log(
-    `[info]: https://ujournal.com.ua/new/post/?postId=${postView.id}`
+    `[info]: Post URL: https://ujournal.com.ua/new/post/?postId=${postView.id}`
   );
 };
 
